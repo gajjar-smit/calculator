698ef6440585b6cf65c99116d692fdf4292ac1b0
rudra-dolphin <rudra@dolphinwebsolutions.com>
2024-08-11 12:51:56 +0530

    improve design of css and make it responsive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let string = ""
-let buttons = document.getElementsByTagName('button');
+let buttons = document.querySelector(".buttons").children
+console.log(buttons)
 Array.from(buttons).forEach((btns) => {
     btns.addEventListener('click', (e) => {
 
@@ -9,7 +10,7 @@ Array.from(buttons).forEach((btns) => {
         switch (e.target.innerHTML) {
             case '=':
                 string = eval(string)
-                document.getElementById("in1").value = string
+                document.getElementById("display").value = string
 
 
                 break;
@@ -23,21 +24,7 @@ Array.from(buttons).forEach((btns) => {
                 string = string + e.target.innerHTML
                 break;
         }
-
-        // if (e.target.innerHTML == '=') {
-        //     string = eval(string)
-        //     document.getElementById("in1").value = string
-        // }
-        // else if (e.target.innerHTML == 'cl') {
-        //     string = ""
-
-        // }
-        // else {
-
-        //     console.log(e.target);
-        //     string = string + e.target.innerHTML
-        // }
-        document.getElementById("in1").value = string
+        document.getElementById("display").value = string
 
     })
 })
